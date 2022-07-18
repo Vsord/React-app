ff3b02559a50a476bcdf4f2580caa65f8b0ba95d
Vsord <stanislav060977@gmail.com>
2022-07-18 14:00:34 -0400

    NavLinks added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import navbarClass from './Navbar.module.css'
+import React from 'react';
+import navbarClass from './Navbar.module.css';
+import {NavLink} from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <div className={navbarClass.navbar}>
             <div className={navbarClass.navbar_menu}>
-                <a href="/profile">Profile</a>
-                <a href="/messages">Messages</a>
-                <a href="/news">News</a>
-                <a href="/feed">Feed</a>
-                <a href="/settings">Settings</a>
+                <NavLink className={(navData) => navData.isActive ? navbarClass.active : ''} to="/profile">Profile</NavLink>
+                <NavLink className={(navData) => navData.isActive ? navbarClass.active : ''} to="/messages">Messages</NavLink>
+                <NavLink className={(navData) => navData.isActive ? navbarClass.active : ''} to="/news">News</NavLink>
+                <NavLink className={(navData) => navData.isActive ? navbarClass.active : ''} to="/feed">Feed</NavLink>
+                <NavLink className={(navData) => navData.isActive ? navbarClass.active : ''} to="/settings">Settings</NavLink>
             </div>
         </div>
     )
